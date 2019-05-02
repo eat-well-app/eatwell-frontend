447cd725943f9e1b9652e6ca6d360dd7e4cf5a94
Casey Bee <caseye.baker@gmail.com>
2019-05-01 22:36:45 -0700

    some styling, some deployment

--- client/src/components/LandingPage/Landing.js
+++ client/src/components/LandingPage/Landing.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Navbar from './Navbar'
-
+import Features from './Features'
 
 function Landing(props) {
   return (
     <div>
-      <Navbar />
+    <Navbar />
+    <Features />
     </div>
   );
 }
--- client/src/components/LandingPage/Navbar.js
+++ client/src/components/LandingPage/Navbar.js
@@ -5,8 +5,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = {
   root: {
@@ -26,7 +24,6 @@ function ButtonAppBar(props) {
           <Typography variant="h6" color="inherit" className={classes.grow}>
             EatWell
           </Typography>
-          <Typography variant="h8" color="inherit" className={classes.grow}>Welcome, User</Typography>
           <section>
           <Button color="inherit">Features</Button>
           <Button color="inherit">Pricing</Button>
